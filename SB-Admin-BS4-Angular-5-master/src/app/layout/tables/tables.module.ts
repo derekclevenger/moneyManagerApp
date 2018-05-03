0cee4b3ec75f8f0e5ea0e96b458e4649b39786b0
Unknown <derek@core10.io>
2018-05-02 22:42:29 -0400

    [feature] expenditures done

--- SB-Admin-BS4-Angular-5-master/src/app/layout/tables/tables.module.ts
+++ SB-Admin-BS4-Angular-5-master/src/app/layout/tables/tables.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, CurrencyPipe} from '@angular/common';
 
 import { TablesRoutingModule } from './tables-routing.module';
 import { TablesComponent } from './tables.component';
 import { PageHeaderModule } from './../../shared';
+import {DashBoardServices} from '../../shared/services/dashboard.service';
+import {BudgetService} from '../../shared/services/budget.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, TablesRoutingModule, PageHeaderModule],
-    declarations: [TablesComponent]
+    imports: [
+        CommonModule,
+        TablesRoutingModule,
+        PageHeaderModule,
+        FormsModule
+    ],
+    declarations: [TablesComponent],
+    providers: [ BudgetService, CurrencyPipe, DashBoardServices ]
+
 })
 export class TablesModule {}
